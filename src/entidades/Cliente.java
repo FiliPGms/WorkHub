24e7c5711e2424e001bebf5e659283beed6d4221
João Filipe <joaofilipevgomes@gmail.com>
2025-12-27 01:27:05 -0300

    protótipo da classe Cliente

---
+++ src/entidades/Cliente.java
@@ -0,0 +1,10 @@
+package entidades;
+import java.time.*;
+
+public class Cliente {
+	private String cpf;
+	private String email;
+	private String nome;
+	private String telefone;
+	private LocalDate dataDeCadastro;
+}
